6592e5b8f16dc10a8b80bd219b04f0702cec2ce0
Roberto S. Ramos Jr <robertosrjr@gmail.com>
2020-05-11 21:43:30 -0300

    swagger

--- user_service/app.py
+++ user_service/app.py
@@ -1,20 +1,47 @@
-from flask import Flask
-from flask_restplus import Api
+from flask import Flask, jsonify
+from flask_swagger import swagger
+from flask_swagger_ui import get_swaggerui_blueprint
+from flask_cors import CORS
 from api.controller.userController import UserController
 
 app = Flask(__name__)
-api = Api(app)
+cors = CORS(app, resources={r"/api/*": {"origins": "*"}})
+
+SWAGGER_URL = '/api/docs'  # URL for exposing Swagger UI (without trailing '/')
+API_URL = 'http://127.0.0.1:5000/spec'  # Our API url (can of course be a local resource)
+
+@app.route("/spec")
+def spec():
+    swag = swagger(app)
+    swag['info']['version'] = "1.0"
+    swag['info']['title'] = "My API"
+    return jsonify(swag)
+
+# Call factory function to create our blueprint
+swaggerui_blueprint = get_swaggerui_blueprint(
+    SWAGGER_URL,  # Swagger UI static files will be mapped to '{SWAGGER_URL}/dist/'
+    API_URL,
+    config={  # Swagger UI config overrides
+        'app_name': "Test application"
+    },
+    # oauth_config={  # OAuth config. See https://github.com/swagger-api/swagger-ui#oauth2-configuration .
+    #    'clientId': "your-client-id",
+    #    'clientSecret': "your-client-secret-if-required",
+    #    'realm': "your-realms",
+    #    'appName': "your-app-name",
+    #    'scopeSeparator': " ",
+    #    'additionalQueryStringParams': {'test': "hello"}
+    # }
+)
+
+# Register blueprint at URL
+# (URL must match the one given to factory function above)
+app.register_blueprint(swaggerui_blueprint, url_prefix=SWAGGER_URL)
 
 controller = UserController()
-@api.route('/users')
-def get(self):
+@app.route('/users')
+def get():
   return controller.get()
-
-'''
-@api.route('/users/<int:id>')
-def get(self, id):
-  return controller.get()
-'''
 
 def initialize_app(app):
   app.config['RESTPLUS_VALIDATE'] = True
